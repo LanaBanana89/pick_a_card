fdf52fc2c1340e75850249d9a269036713d71c85
LanaBanana <s.glazyrina@mail.ru>
2017-09-17 12:08:31 +0300

    Print a pictures of suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,15 +1,18 @@
-require_relative './data/suits.txt'
+current_path = File.dirname(__FILE__)
 
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 #suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
 
-if File.exist?('data/suits.txt')
-  f = File.new('data/suits.txt',"r:UTF-8")
+if File.exist?(current_path + "/data/suits.txt")
+  f = File.new(current_path + "/data/suits.txt","r:UTF-8")
   file = f.readlines
   f.close
+
+else
+  puts "Файл не найден"
 
 end
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{file.sample}"
+puts "#{values.sample}#{file.sample}"
