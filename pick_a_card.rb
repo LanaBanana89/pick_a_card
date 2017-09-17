8c5000e6f4bcaa1fc49e51a4fb70831cac18ea29
LanaBanana <s.glazyrina@mail.ru>
2017-09-17 03:11:34 +0300

    Version 4

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,15 @@
+require_relative './data/suits.txt'
+
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+#suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+
+if File.exist?('data/suits.txt')
+  f = File.new('data/suits.txt',"r:UTF-8")
+  file = f.readlines
+  f.close
+
+end
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} of #{file.sample}"
