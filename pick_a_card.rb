5ca19c16e8aa7707fd61fdd63c1dc427f8a89235
LanaBanana89 <32016884+LanaBanana89@users.noreply.github.com>
2018-03-28 18:31:37 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,14 +1,4 @@
 # Программа, которая выбирает произвольную карту игральной колоды
-
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
 puts 'Pick a Card. (c) goodprogrammer.ru'
 
 # Массивы с наборами мастей и достоинств
