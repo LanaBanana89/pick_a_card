1bdc8e2c31c605f68c5395493095af880d5f1e39
LanaBanana89 <s.glazyrina@mail.ru>
2018-03-28 20:55:00 +0300

    Title & copyright

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -9,6 +9,8 @@ if (Gem.win_platform?)
   end
 end
 
+puts 'Pick a Card. (c) goodprogrammer.ru'
+
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['♦','♠','♥ ','♣']
