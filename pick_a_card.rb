8e234e088f7c919b411bfb5bc3d3ec36dc5c3041
LanaBanana89 <s.glazyrina@mail.ru>
2018-03-29 21:34:01 +0300

    Correct errors

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -9,25 +9,12 @@ if (Gem.win_platform?)
   end
 end
 
-puts 'Pick a Card. (c) goodprogrammer.ru'
 
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['♦','♠','♥ ','♣']
 
-cards = []
+# Выведем произвольную карты, выбрав по одному элементу из массивов
+puts "#{values.sample}#{suits.sample}"
 
-values.each do |value|
-  suits.each do |suit|
-    cards << "#{value} of #{suit}"
-  end
-end
 
-cards.shuffle!
-
-puts "How many cards do you pick?"
-number = STDIN.gets.to_i
-
-number.times do
-  puts cards.pop
-end
